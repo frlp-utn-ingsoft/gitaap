df03afd5ebe917c96e4a610dfb3e54aa419a257b
juancruzchemin <juancruzcheminv@gmail.com>
2021-05-05 00:28:14 -0300

    Arregla de fecha en form

--- server/models/movement.js
+++ server/models/movement.js
@@ -62,14 +62,17 @@ const getAllMovements = (limit, skip, type) => {
  * Crear un movimiento nuevo.
  * Parámetro data: JSON con los atributos a crear.
  *
+ * Se modifica la fecha para que devuelva la ingresada por pantalla
  */
 const createMovement = ({
-    date = '01/01/2021',
+    date = new Date('01/01/2021'),
     amount = 0.0,
     type = MovementType.EXPENSE,
     category = '',
 } = {}) => {
-    date = new Date()
+    
+    
+
     return Movement.create({ date, amount, type, category });
 };
 
