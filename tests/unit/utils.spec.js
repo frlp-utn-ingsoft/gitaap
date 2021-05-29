374c1691ca907edb437294b363cbe1137ac82506
unknown <fransuasna@gmail.com>
2021-05-29 20:27:50 -0300

    Testeo: método formatDate

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -1,14 +1,13 @@
-const { monefy } = require('../../client/js/utils.js');
+const { formatDate } = require('../../client/js/utils.js');
 
-test('Should convert 1000 into "1.000"', async () => {
-    expect(monefy(1000)).toBe('1.000');
+
+describe('Método formatDate', () =>{
+
+    const fecha = new Date('August 20, 2020 23:15:33');
+    const fechaEsperada = '20/8/2020';
+    it('Dada una fecha, debería devolverla en el formato correcto', () => {
+        expect(formatDate(fecha)).toBe(fechaEsperada);
+    });
+
+
 });
-
-test('Should convert 100 into "100"', async () => {
-    expect(monefy(100)).toBe('100');
-});
-
-test('Should convert undefined into ""', async () => {
-    expect(monefy(undefined)).toBe('');
-});
-
