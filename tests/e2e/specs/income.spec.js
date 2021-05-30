880d25d6c232043fec2a30252725aa7d395f8d2a
Florencia_Fried <florencia.d.fried@gmail.com>
2021-05-30 20:54:17 -0300

    Arreglé un error en income.spec.js que le tiraba a mis compañeros

--- tests/e2e/specs/income.spec.js
+++ tests/e2e/specs/income.spec.js
@@ -36,6 +36,7 @@ describe('Ingresos Test', () => {
         cy.get('input[name=date]').type('2021-04-26');
         cy.get('input[name=category]').type('Bono');
         cy.get('input[name=amount]').type('100000');
+        cy.wait(3000);
         cy.contains('Guardar').click();
 
         const stub = cy.stub();
